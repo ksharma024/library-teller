ebee15bffd93da4fa04c4bc72cbb842ad79258d2
James M. Allen <james.m.allen@gmail.com>
2016-07-01 09:51:44 -0400

    Done with part 1

---
+++ Library/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book : Media
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                return 14;
+            }
+            // dummy set method - doesn't do anything
+            set
+            {
+            }
+
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Book {0} ({1})", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on: {0}", ReturnDueDate());
+
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentedDate.AddDays(RentalLength);
+        }
+    }
+}
---
+++ Library/Magazine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Magazine : Media
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                return 2;
+            }
+            // dummy set method - doesn't do anything
+            set
+            {
+            }
+
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Magazine {0} ({1})", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on: {0}", ReturnDueDate());
+
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentedDate.AddDays(RentalLength);
+        }
+    }
+}
